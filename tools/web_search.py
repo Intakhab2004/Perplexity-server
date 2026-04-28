ea83a3d1c9271b19acb08e4016d85d9f26bfbeed
Intakhab2004 <ikbalam4648@gmail.com>
2026-04-28 19:48:13 +0530

    Implemented tavily api for web search

---
+++ tools/web_search.py
@@ -0,0 +1,28 @@
+from tavily import TavilyClient
+from helpers.config import settings
+
+tavily_client = TavilyClient(api_key=settings.TAVILY_API_KEY)
+
+def search_query(sub_query: str): 
+    response = tavily_client.search(
+        query=sub_query,
+        max_results=5,
+        search_depth='advanced',
+        exclude_domains=["facebook.com", "instagram.com", "twitter.com", "tiktok.com", "pinterest.com", "flipboard.com"]
+    )
+
+    # Structuring output
+    data = response["results"]
+    formatted_data = [
+        {
+            "url": item["url"],
+            "title": item["title"],
+            "content": item["content"]
+        }
+        for item in data[:3]
+    ]
+
+    return formatted_data
+
+
+     
