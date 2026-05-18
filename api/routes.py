91bb9fc1deca222013983cd6e82b3908a9c9e023
Intakhab2004 <ikbalam4648@gmail.com>
2026-05-18 20:54:08 +0530

    Resolved bugs in embedding generation

--- api/routes.py
+++ api/routes.py
@@ -8,6 +8,7 @@ from agents.search_agent import search
 from services.chunking_service import generate_chunks
 from services.vector_store import store_embeddings
 from agents.answer_agent import generate_answer
+import time
 
 router = APIRouter()
 
@@ -26,26 +27,43 @@ class QueryRequest(BaseModel):
 @router.post("/ask")
 def ask_question(req: QueryRequest):
     try:
+        t1 = time.time()
+
         plan = generate_plan(query=req.query, user_id=req.user_id)
         if not plan:
             raise ValueError("Error occured while generating plan")
+        
+        print("Subqueries:", time.time()-t1)
+        t2 = time.time()
         
         search_agent_content = search(plan)
         if not search_agent_content:
             raise ValueError("Error occured during web search")
         
+        print("search_agent_content:", time.time()-t2)
+        t3 = time.time()
+        
         chunks = generate_chunks(search_agent_content)
         if not chunks:
             raise ValueError("Something went wrong while generating the chunks")
+        
+        print("generate_chunks:", time.time()-t3)
+        t4 = time.time()
+        
         
         info = store_embeddings(chunks)
         if not info == "completed":
             raise ValueError("Something went wrong while storing embeddings")
         
+        print("store_embeddings:", time.time()-t4)
+        t5 = time.time()
+        
         result = generate_answer(req.query)
         if not result:
             raise ValueError("Something went wrong while answer llm call")
         
+        print("generate_answer:", time.time()-t5)
+
         return {
             "success": True,
             "data": result
